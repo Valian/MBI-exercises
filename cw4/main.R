a4e510554bb08284425495405a5f333586e13abb
Mateusz Chydziński <chydzio@gmail.com>
2018-06-16 16:08:46 +0200

    liczenie pokrycia

--- cw4/main.R
+++ cw4/main.R
@@ -13,30 +13,29 @@ library(CODEX)
 #set number of available cores
 cores <- 4
 
-
 # calc coverage
-
-# preview bedFile
-fread(bedFile)
-
 dirPath <- system.file("extdata", package = "WES.1KG.WUGSC")
 bamFile <- list.files(dirPath, pattern = '*.bam$')
+
+#show bedfile
+bedFileContent <- fread(bedFile)
+
 bamdir <- file.path(dirPath, bamFile)
 sampname <- as.matrix(read.table(file.path(dirPath, "sampname")))
 bedFile <- file.path(dirPath, "chr22_400_to_500.bed")
 chr <- 22
 bambedObj <- getbambed(bamdir = bamdir, bedFile = bedFile,
-sampname = sampname, projectname = "CODEX_demo", chr)
+            sampname = sampname, projectname = "CODEX_demo", chr)
 bamdir <- bambedObj$bamdir; sampname <- bambedObj$sampname
 ref <- bambedObj$ref; projectname <- bambedObj$projectname; chr <- bambedObj$chr
 coverageObj <- getcoverage(bambedObj, mapqthres = 20)
 
 # information about reads for each region
 Y <- coverageObj$Y; readlength <- coverageObj$readlength
-Objekt Y zawiera dane na temat liczby odczytów dla ka»dego regionu; próbki s¡ zdenio-
-wane w kolejnych kolumnach; regiony w wierszach.
+#Objekt Y zawiera dane na temat liczby odczytów dla ka»dego regionu; próbki s¡ zdenio-
+#wane w kolejnych kolumnach; regiony w wierszach.
 Y_ac <- apply(Y, 2,function(x){(100*x)/width(ref)})
 colnames(Y_ac) <- sampname
 
 # coverage depth statistics
-summary(apply(Y_ac, 2, median))
+summary <- summary(apply(Y_ac, 2, median))
