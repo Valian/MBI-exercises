7479d559b72671a9bfa731ba061f032729cb602b
Valian <jakub.skalecki@gmail.com>
2019-12-03 23:58:39 +0100

    added outputs and work in progress of doc

--- cw2/README.md
+++ cw2/README.md
@@ -1,9 +1,9 @@
 # Order
 
 ```
-./download.sh
-./extract_sequence.py
-./mask_genome.sh
-./structural_annotation.sh
-./get_sequence_for_blastx.py
+bash download.sh
+python3 extract_sequence.py > outputs/extract_sequence_output.txt
+bash mask_genome.sh > outputs/mask_genome_output.txt 
+bash structural_annotation.sh > outputs/structural_annotation_output.txt 2>&1
+python3 get_sequence_for_blastx.py > outputs/get_sequence_for_blastx_output.txt
 ```
--- cw2/extract_sequence.py
+++ cw2/extract_sequence.py
@@ -1,5 +1,7 @@
 #!/bin/env python3
 import re
+import random
+
 
 MIN_SCAFFOLD_LENGTH = 100 * 1000 #  100 kbp
 
@@ -8,15 +10,14 @@ with open('/tmp/mbi2/genome.fa', 'r') as f:
     scaffolds = list(filter(lambda x: x[:len(prefix)]==prefix, f.read().split('>')))
     print('found', len(scaffolds), 'scaffolds')
     regex = r'HDID_scaffold(\d+) length=(\d+)'
-    regex =re.compile(regex)
-    for s in scaffolds:
-        match = regex.match(s)
-        scaffold_id, scaffold_length = match.group(1,2)
-        if int(scaffold_length) >= MIN_SCAFFOLD_LENGTH:
-            print('got scaffold', scaffold_id, 'of length', scaffold_length)
-            break
+    regex = re.compile(regex)
+    valid_scaffolds = [s for s in scaffolds if int(regex.match(s).group(2)) >= MIN_SCAFFOLD_LENGTH]
+    print('found ', len(valid_scaffolds), ' scaffolds longer than ', MIN_SCAFFOLD_LENGTH)
+    chosen_scaffold = random.choice(valid_scaffolds)
+    scaffold_id, scaffold_length = regex.match(chosen_scaffold).group(1, 2)
+    print('got scaffold', scaffold_id, 'of length', scaffold_length)
 
-with open('/tmp/mbi2/single_scaffold.fa', 'w') as f:
-    # header = (s.split('\n')[0]).split(' ')[0]
-    # f.write(header + '\n' + '\n'.join(s.split('\n')[1:]))
-    f.write('>'+s)
+    with open('/tmp/mbi2/single_scaffold.fa', 'w') as f:
+        # header = (s.split('\n')[0]).split(' ')[0]
+        # f.write(header + '\n' + '\n'.join(s.split('\n')[1:]))
+        f.write('>' + chosen_scaffold)
